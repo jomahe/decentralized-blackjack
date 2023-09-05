34c05013195d2566dceb93ff8b5d9c6244fcef3f
jomahe <jomahe@protonmail.com>
2023-09-04 19:39:26 -0700

    Dealer added

--- contracts/Blackjack.sol
+++ contracts/Blackjack.sol
@@ -3,32 +3,44 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import {Dealer} from "./Dealer.sol";
 
 contract Blackjack is ERC721, Ownable {
     struct GameData {
         uint256 betAmount;
+        uint256 lastRequestId;
         bool firstTurn;
         bool insurance;
         uint8[4] handSums;
         uint8[4][] hands;
+        uint8 dealerHand;
     }
     GameData gameData;
+    Dealer dealer;
     address immutable vault;
     uint256 public totalSupply;
 
-    constructor(address _vault) payable ERC721("blackjack", "BJK") {
+    constructor(
+        address _vault,
+        address _dealer
+    ) payable ERC721("blackjack", "BJK") {
         require(msg.value >= 0, "Need to place an initial bet!");
         /*
            Set up the vault, generate random numbers for the hands and
            populate game data
         */
         vault = _vault;
+        dealer = Dealer(_dealer);
 
+        gameData.lastRequestId = dealer.requestRandomWords();
         gameData.betAmount = msg.value;
         gameData.insurance = false;
-        gameData.firstTurn = true;
+        gameData.firstTurn = false;
+
+        // TODO: implement the startGame() function, requiring that the NFT is sent and we mint a new game state to the user's wallet
     }
+
+    function startGame() external {}
 
     function hit(bool _insurance, uint256 handNum) external payable {
         // Function logic
---
+++ contracts/Dealer.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+contract Dealer is VRFConsumerBaseV2 {
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+    struct RequestStatus {
+        bool fulfilled;
+        bool exists;
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus) public requests;
+    VRFCoordinatorV2Interface coordinator;
+    uint64 subscriptionId;
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+    bytes32 keyHash =
+        0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function setOwner(address _newOwner) external onlyOwner {
+        owner = _newOwner;
+    }
+
+    /**
+     * HARDCODED FOR SEPOLIA
+     * COORDINATOR: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
+     */
+    constructor(
+        uint64 _subscriptionId
+    ) VRFConsumerBaseV2(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625) {
+        coordinator = VRFCoordinatorV2Interface(
+            0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
+        );
+        subscriptionId = _subscriptionId;
+        owner = msg.sender;
+    }
+
+    // Assumes the subscription is funded sufficiently.
+    function requestRandomWords()
+        external
+        onlyOwner
+        returns (uint256 requestId)
+    {
+        // Will revert if subscription is not set and funded.
+        requestId = coordinator.requestRandomWords(
+            keyHash,
+            subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(requests[_requestId].exists, "request not found");
+        requests[_requestId].fulfilled = true;
+        requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(requests[_requestId].exists, "request not found");
+        RequestStatus memory request = requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+}
